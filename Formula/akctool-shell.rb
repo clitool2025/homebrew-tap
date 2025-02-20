7f960bd86d4399f2d157ce7de95b18c59f347765
anker <leo.ji@anker-in.com>
2025-02-20 18:01:41 +0800

    update

--- Formula/akctool-shell.rb
+++ Formula/akctool-shell.rb
@@ -1,8 +1,8 @@
 class AkctoolShell < Formula
   desc "A simple and powerful shell script for automation"
   homepage "https://github.com/clitool2025/akcTool"
-  url "https://github.com/clitool2025/akcTool/archive/v1.0.0.tar.gz"
-  sha256 "33942ac194d583958ed82e56a1e6b985ae4621d2e42dfec380facb29be94b121"
+  url "https://github.com/clitool2025/akcTool/archive/v1.0.1.tar.gz"
+  sha256 "6e0b411400aae10d07548a970b4f79107739187c445c86c6a1be574581b95145"
   
   def install
     bin.install "akcTool" => "akcTool"
